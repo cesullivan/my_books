80d260cafe2429106c371e22b8816f6bcf9c8732
Cathy <cesullivan@gmail.com>
2012-01-18 13:43:22 -0800

    adding a bunch of files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBooks::Application.config.secret_token = '12d9815f66b27db1a1eb6fe0d1f0539b154f1ace1a887477d564fb71ec03a5dea794b256b08a1b24e3a031069a7bf45b9756159dbdf956a95e06f683c43a9336'
